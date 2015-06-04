2bea154f65333e00f035bec1f6af0dbfd2160760
sycLin <b01902044@ntu.edu.tw>
2015-06-04 17:25:40 +0800

    +2 classes: NGRAM and GRAM

--- phase 2/main.py
+++ phase 2/main.py
@@ -8,14 +8,45 @@ import sys
 #################### Class Definitions ####################
 
 class NGRAM:
+
 	def __init__(self):
-		pass
+		self.gramList = []
+		self.gramCount = 0
 	
 	# build up the n-gram model from lines of data
 	def build(self, linesOfData):
 		# add start symbols and end symbols
 		# process the linesOfData
 		pass
+
+	# count total occurences of all gram instances
+	def countAll(self):
+		total = 0
+		for i in self.gramList:
+			total = total + i.count
+		self.gramCount = total
+		return total
+
+	# check if the tuple already exists in gramList
+	def hasGram(self, gramTuple):
+		pass
+
+	# add a gram instance of the tuple
+	def addGram(self, gramTuple):
+		pass
+
+	# return the count corresponding to the given tuple
+	def getGramCount(self, gramTuple):
+		pass
+
+class GRAM:
+
+	def __init__(self, gramTuple):
+		self.Content = gramTuple
+		self.count = 0
+	
+	def __repr__(self):
+		return ("This gram: "+self.Content+" Count = "+self.Count)
 
 pass
 
@@ -55,6 +86,32 @@ except:
 	sys.exit(1)
 
 print("files both opened successfully!")
+
+###############
+# build NGRAM #
+###############
+
+uniGram = NGRAM()
+biGram = NGRAM()
+triGram = NGRAM()
+
+########################
+# process the raw data #
+########################
+
+i = 0
+while True:
+	rawErrorLine = trainFile.readline()
+	if rawErrorLine == "":
+		break
+	rawCorrectLine = trainFile.readline()
+	print rawErrorLine
+	print rawCorrectLine
+	i += 1
+	if i == 10:
+		break
+
+
 
 ###############
 # close files #
