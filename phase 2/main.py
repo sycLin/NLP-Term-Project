ef133400d2f11f50654140f4e712b77dcfc50760
sycLin <b01902044@ntu.edu.tw>
2015-06-04 10:18:16 +0800

    + structure established

---
+++ phase 2/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#################### Import Modules ####################
+import os
+import sys
+
+#################### Class Definitions ####################
+
+pass
+
+#################### Helper Functions ####################
+
+pass
+
+#################### Variables Declaration ####################
+
+pass
+
+#################### Main Program ####################
+
+pass
