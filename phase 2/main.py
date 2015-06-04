61198910f04e5d7a695fd624c19d70deb9b814f1
sycLin <b01902044@ntu.edu.tw>
2015-06-05 01:34:30 +0800

    +class implementation filled, +negative NGRAM instances created

--- phase 2/main.py
+++ phase 2/main.py
@@ -9,41 +9,70 @@ import sys
 
 class NGRAM:
 
-	def __init__(self):
+	def __init__(self, n):
+		self.N = n
 		self.gramList = []
 		self.gramCount = 0
 	
-	# build up the n-gram model from lines of data
-	def build(self, linesOfData):
+	# build up the n-gram model from taglist
+	def build(self, tagList):
+
 		# add start symbols and end symbols
-		# process the linesOfData
-		pass
+		for i in range(self.N - 1):
+			tagList.insert(0, "^")
+			tagList.append("$")
+
+		# process the taglist to get tuple
+		length = len(tagList)
+		tmp = []
+		for i in range(length - self.N + 1):
+			tmp.append(tagList[i])
+		gramTuple = tuple(tmp)
+
+		# add this tuple to NGRAM
+		self.addGram(gramTuple)
 
 	# count total occurences of all gram instances
 	def countAll(self):
 		total = 0
 		for i in self.gramList:
-			total = total + i.count
+			total = total + i.Count
 		self.gramCount = total
 		return total
 
 	# check if the tuple already exists in gramList
+	# 1) return the GRAM instance if found.
+	# 2) return False if not found.
 	def hasGram(self, gramTuple):
-		pass
+		for i in self.gramList:
+			if i.content == gramTuple:
+				return i
+		return False
 
 	# add a gram instance of the tuple
+	# 1) if didn't exist, creat and insert.
+	# 2) if already exists, count += 1
 	def addGram(self, gramTuple):
-		pass
+		tmp = self.hasGram(gramTuple)
+		if tmp == False: # does not exist, yet.
+			# create a GRAM instance, and insert.
+			newGRAMInstance = GRAM(gramTuple)
+			self.gramList.append(newGRAMInstance)
+		else: # already exists
+			tmp.Count += 1
 
 	# return the count corresponding to the given tuple
 	def getGramCount(self, gramTuple):
-		pass
+		tmp = self.hasGram(gramTuple)
+		if tmp == False:
+			return 0
+		return tmp.Count
 
 class GRAM:
 
 	def __init__(self, gramTuple):
 		self.Content = gramTuple
-		self.count = 0
+		self.Count = 1
 	
 	def __repr__(self):
 		return ("This gram: "+self.Content+" Count = "+self.Count)
@@ -100,9 +129,15 @@ print("files both opened successfully!")
 # build NGRAM #
 ###############
 
-uniGram = NGRAM()
-biGram = NGRAM()
-triGram = NGRAM()
+# positive ones
+uniGram = NGRAM(1)
+biGram = NGRAM(2)
+triGram = NGRAM(3)
+
+# negative ones
+uniGramNeg = NGRAM(1)
+biGramNeg = NGRAM(2)
+triGramNeg = NGRAM(3)
 
 ########################
 # process the raw data #
@@ -110,12 +145,26 @@ triGram = NGRAM()
 
 i = 0
 while True:
+
+	# get lines from p2.*.tagged.txt two by two
 	rawErrorLine = trainFile.readline()
 	if rawErrorLine == "": # EOF reached
 		break
 	rawCorrectLine = trainFile.readline()
-	print process_raw_line(rawErrorLine)
-	print process_raw_line(rawCorrectLine)
+
+	# process the line, to get the POS tags sequence only
+	negList = process_raw_line(rawErrorLine)
+	posList = process_raw_line(rawCorrectLine)
+
+	# build the NGRAMs
+	uniGram.build(posList)
+	biGram.build(posList)
+	triGram.build(posList)
+
+	uniGramNeg.build(negList)
+	biGramNeg.build(negList)
+	triGramNeg.build(negList)
+
 	i += 1
 	if i == 10:
 		break
