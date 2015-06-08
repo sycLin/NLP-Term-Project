3c05d8e13a40495036de4076c1dcdac868d2b9fd
sycLin <b01902044@ntu.edu.tw>
2015-06-08 21:44:02 +0800

    +fix the test file creation: should take odd-numbered lines only

--- phase 2/src/slice_train.sh
+++ phase 2/src/slice_train.sh
@@ -35,7 +35,8 @@ tail -$(($linecount - $portion)) tmp_tmp.txt > tmp_portion_5.txt
 rm -f tmp_tmp.txt tmp_tmp2.txt
 
 # create the FIRST TEST: using first 1/5 as held-out dataset
-cat tmp_portion_1.txt > tmp_test_1.txt
+cat tmp_portion_1.txt > XDDTMP.txt
+awk 'NR % 2 == 1' XDDTMP.txt > tmp_test_1.txt
 cat tmp_portion_2.txt > tmp_train_1.txt
 cat tmp_portion_3.txt >> tmp_train_1.txt
 cat tmp_portion_4.txt >> tmp_train_1.txt
@@ -43,7 +44,8 @@ cat tmp_portion_5.txt >> tmp_train_1.txt
 
 # create the SECOND TEST: using second 1/5 as held-out dataset
 cat tmp_portion_1.txt > tmp_train_2.txt
-cat tmp_portion_2.txt > tmp_test_2.txt
+cat tmp_portion_2.txt > XDDTMP.txt
+awk 'NR % 2 == 1' XDDTMP.txt > tmp_test_2.txt
 cat tmp_portion_3.txt >> tmp_train_2.txt
 cat tmp_portion_4.txt >> tmp_train_2.txt
 cat tmp_portion_5.txt >> tmp_train_2.txt
@@ -51,7 +53,8 @@ cat tmp_portion_5.txt >> tmp_train_2.txt
 # create the THIRD TEST: using third 1/5 as held-out dataset
 cat tmp_portion_1.txt > tmp_train_3.txt
 cat tmp_portion_2.txt >> tmp_train_3.txt
-cat tmp_portion_3.txt > tmp_test_3.txt
+cat tmp_portion_3.txt > XDDTMP.txt
+awk 'NR % 2 == 1' XDDTMP.txt > tmp_test_3.txt
 cat tmp_portion_4.txt >> tmp_train_3.txt
 cat tmp_portion_5.txt >> tmp_train_3.txt
 
@@ -59,7 +62,8 @@ cat tmp_portion_5.txt >> tmp_train_3.txt
 cat tmp_portion_1.txt > tmp_train_4.txt
 cat tmp_portion_2.txt >> tmp_train_4.txt
 cat tmp_portion_3.txt >> tmp_train_4.txt
-cat tmp_portion_4.txt > tmp_test_4.txt
+cat tmp_portion_4.txt > XDDTMP.txt
+awk 'NR % 2 == 1' XDDTMP.txt > tmp_test_4.txt
 cat tmp_portion_5.txt >> tmp_train_4.txt
 
 # create the FIRST TEST: using first 1/5 as held-out dataset
@@ -67,7 +71,8 @@ cat tmp_portion_1.txt > tmp_train_5.txt
 cat tmp_portion_2.txt >> tmp_train_5.txt
 cat tmp_portion_3.txt >> tmp_train_5.txt
 cat tmp_portion_4.txt >> tmp_train_5.txt
-cat tmp_portion_5.txt > tmp_test_5.txt
+cat tmp_portion_5.txt > XDDTMP.txt
+awk 'NR % 2 == 1' XDDTMP.txt > tmp_test_5.txt
 
 # delete the temporary files
 rm -f tmp_portion_1.txt
@@ -75,6 +80,7 @@ rm -f tmp_portion_2.txt
 rm -f tmp_portion_3.txt
 rm -f tmp_portion_4.txt
 rm -f tmp_portion_5.txt
+rm -f XDDTMP.txt
 
 # move the files to ../data
 mv tmp_test_*.txt ../data/
