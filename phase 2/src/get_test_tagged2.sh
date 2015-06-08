38aae15905bd11ef45ce92ccf39a421b15d72215
sycLin <b01902044@ntu.edu.tw>
2015-06-08 23:43:49 +0800

    +supporting SCWS

---
+++ phase 2/src/get_test_tagged2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SOURCE_FILE='train_cut.c'
+
+# compile the train_cut.c for scws
+INC_FLAG='-I/usr/local/scws/include/scws/'
+LIB_FLAG='-L/usr/local/scws/lib/'
+LINK_FLAG='-lscws'
+OUTPUT_NAME='a.out'
+
+gcc $INC_FLAG $LIB_FLAG $SOURCE_FILE $LINK_FLAG -o $OUTPUT_NAME
+
+# tag the training data
+
+./$OUTPUT_NAME < ../data/p2.test.sentences.txt > ../data/p2.test.tagged.txt
+
+rm -f $OUTPUT_NAME
