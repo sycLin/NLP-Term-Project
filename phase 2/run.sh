c006ef38c024de55f56e9c6cbf77ff20cac549df
sycLin <b01902044@ntu.edu.tw>
2015-06-08 22:08:44 +0800

    +shell script for running real test data

---
+++ phase 2/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MAIN_FILE='./src/main.py'
+TRAIN_FILE='./data/p2.train.tagged.txt'
+TEST_FILE='./data/p2.test.tagged.txt'
+
+# check if the directory testResult/ exists or not
+if ! [ -d testResult/ ];then
+	mkdir testResult
+fi
+
+# run it!!!
+
+$MAIN_FILE $TRAIN_FILE $TEST_FILE > testResult/result.txt
+
