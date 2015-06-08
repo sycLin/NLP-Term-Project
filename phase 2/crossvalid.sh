34e172cc9e8757f1bb6222bfe3b82ec971e9c3d8
sycLin <b01902044@ntu.edu.tw>
2015-06-08 21:24:30 +0800

    +fix bug: file naming

--- phase 2/crossvalid.sh
+++ phase 2/crossvalid.sh
@@ -2,16 +2,10 @@
 
 # files required
 MAIN_FILE="./src/main.py"
-TRAIN_FILE="./data/p2.train.txt"
 
 # check those files required
 if ! [ -f $MAIN_FILE ]; then
 	echo "Error: $MAIN_FILE does not exist!"
-	exit 1
-fi
-
-if ! [ -f $TRAIN_FILE ]; then
-	echo "Error: $TRAIN_FILE does not exist!"
 	exit 1
 fi
 
@@ -23,6 +17,6 @@ fi
 # run 5 times
 for i in `seq 5`
 do
-	$MAIN_FILE ./data/tmp_train_$i.txt ./data/tmp_test_$i.txt > ./data/tmp_answer_$i.txt
+	$MAIN_FILE ./data/tmp_train_$i.txt ./data/tmp_test_$i.txt > ./cvResult/tmp_answer_$i.txt
 done
 
