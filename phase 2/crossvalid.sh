4a4687f830773e9241464ece5e17af62ff511184
sycLin <b01902044@ntu.edu.tw>
2015-06-08 23:57:02 +0800

    +support for both SCWS and Stanford

--- phase 2/crossvalid.sh
+++ phase 2/crossvalid.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# check argument
+
+if [ $# != 1 ]; then
+	echo "Usage: $0 <scws|stanford>"
+	exit 1
+elif [ $1 == "scws" ]; then
+	TAG_SCRIPT='get_train_tagged2.sh'
+elif [ $1 == "stanford" ]; then
+	TAG_SCRIPT='get_train_tagged.sh'
+else
+	echo "Usage: $0 <scws|stanford>"
+	exit 1
+fi
+
+# tag the training data
+cd src
+./$TAG_SCRIPT
+cd ..
+
+exit 1
+
 # files required
 MAIN_FILE="./src/main.py"
 
