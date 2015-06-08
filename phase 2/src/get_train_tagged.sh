2d5b2f76b9c8f182f0318fe136716374e7220b43
sycLin <b01902044@ntu.edu.tw>
2015-06-08 21:06:10 +0800

    +fix for new DIR hierarchy

--- phase 2/src/get_train_tagged.sh
+++ phase 2/src/get_train_tagged.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # 1) get the sentences
-cat p2.train.txt | cut -d $'\t' -f 2,3 | tr $'\t' $'\n' > p2.train.sentences.txt
+cat ../data/p2.train.txt | cut -d $'\t' -f 2,3 | tr $'\t' $'\n' > ../data/p2.train.sentences.txt
 
 # 2) segment them
 
-./stanford-segmenter-2015-04-20/segment.sh -k pku p2.train.sentences.txt UTF-8 0 > p2.train.segmented.txt
+../stanford-segmenter-2015-04-20/segment.sh -k pku ../data/p2.train.sentences.txt UTF-8 0 > ../data/p2.train.segmented.txt
 
 # 3) tag them
 
-cp p2.train.segmented.txt stanford-postagger-full-2015-04-20/
-cd stanford-postagger-full-2015-04-20/
+cp ../data/p2.train.segmented.txt ../stanford-postagger-full-2015-04-20/
+cd ../stanford-postagger-full-2015-04-20/
 ./stanford-postagger.sh models/chinese-distsim.tagger p2.train.segmented.txt > p2.train.tagged.txt
-cp p2.train.tagged.txt ../
+cp p2.train.tagged.txt ../data/
 rm p2.train.tagged.txt p2.train.segmented.txt
-cd ../
+cd ../src/
 
